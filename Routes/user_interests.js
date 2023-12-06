f964406e9e93e233a3d183e45afe0b0faf351e67
asmajehad20 <126648311+asmajehad20@users.noreply.github.com>
2023-12-06 21:53:09 +0200

    Delete Routes directory

--- Routes/user_interests.js
+++
@@ -1,9 +0,0 @@
-// routes/interestRoute.js
-const express = require('express');
-const InterestController = require('../Controller/user_interestsController');
-
-const router = express.Router();
-
-router.post('/add-interest', InterestController.addInterest);
-
-module.exports = router;
